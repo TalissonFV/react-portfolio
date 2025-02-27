384f2ee0ac37cb4372bbe573ba7c66615291221c
talissonfv <talissonfvilela@gmail.com>
2025-02-27 01:40:38 -0300

    Alterado para link funcionar ao clicar em toda parte da div

--- src/components/Formacao.tsx
+++ src/components/Formacao.tsx
@@ -1,4 +1,3 @@
-// components/Formacao.tsx
 const Formacao = () => {
   const formacoes = [
     {
@@ -6,33 +5,44 @@ const Formacao = () => {
       curso: "Bacharel em Sistemas de Informação",
       periodo: "Concluído em 2019",
       logo: "https://imagens.unisanta.br/logos/logo.jpg",
-      certificado: "https://www.e-diploma.com.br/Verificar/Autenticidade.aspx?id=394225|7D53C9A1E82784F0DA5B2704E0182A|8386"
-    }
-  ]
+      certificado:
+        "https://www.e-diploma.com.br/Verificar/Autenticidade.aspx?id=394225|7D53C9A1E82784F0DA5B2704E0182A|8386",
+    },
+  ];
 
   return (
     <section id="formacao" className="section-spacing bg-light">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 text-secondary">Formação Acadêmica</h2>
+        <h2 className="text-3xl font-bold mb-8 text-secondary">
+          Formação Acadêmica
+        </h2>
         <div className="space-y-8">
           {formacoes.map((formacao, index) => (
-            <div key={index} className="card-style flex items-center gap-6 bg-white">
-              <img
-                src={formacao.logo}
-                className="w-24 h-24 "
-                alt={formacao.instituicao}
-              />
+            <div key={index}>
               <a href={formacao.certificado}>
-                <h3 className="text-xl font-bold mb-2 text-dark">{formacao.curso}</h3>
-                <p className="font-semibold mb-2 text-primary">{formacao.instituicao}</p>
-                <p className="text-gray-600">{formacao.periodo}</p>
+                <div className="card-style flex items-center gap-6 bg-white">
+                  <img
+                    src={formacao.logo}
+                    className="w-24 h-24 "
+                    alt={formacao.instituicao}
+                  />
+                  <span>
+                    <h3 className="text-xl font-bold mb-2 text-dark">
+                      {formacao.curso}
+                    </h3>
+                    <p className="font-semibold mb-2 text-primary">
+                      {formacao.instituicao}
+                    </p>
+                    <p className="text-gray-600">{formacao.periodo}</p>
+                  </span>
+                </div>
               </a>
             </div>
           ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Formacao
+export default Formacao;
