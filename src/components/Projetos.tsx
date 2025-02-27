ce159d3836e821dedd38993c462a2c3434f49b76
talissonfv <talissonfvilela@gmail.com>
2025-02-26 23:22:02 -0300

    Adicionado animação ao passar mouse em cima dos projetos realizados

--- src/components/Projetos.tsx
+++ src/components/Projetos.tsx
@@ -51,14 +51,14 @@ const Projetos = () => {
   ];
 
   return (
-    <section id="projetos" className="section-spacing bg-beige">
+    <section id="projetos" className="section-spacing">
       <div className="container mx-auto px-4 card-style bg-[#f09d51]">
         <h2 className="text-3xl font-bold mb-8 text-secondary">Projetos</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projetos.map((projeto, index) => (
             <div
               key={index}
-              className="card-style hover:shadow-xl transition-shadow flex flex-col"
+              className="card-style hover:shadow-2xl transition-shadow transform hover:scale-105 flex flex-col bg-white"
             >
               {projeto.imagem && (
                 <img
